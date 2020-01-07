c92bb9b0d093470aa85e7498d187703a3003c4f2
valentin lafranca <valentin.lafranca@viacesi.fr>
2020-01-06 19:07:27 -0500

    fix Ionic ion-input handling

--- projects/ngx-autocom-place/src/lib/ngx-autocom-place.directive.ts
+++ projects/ngx-autocom-place/src/lib/ngx-autocom-place.directive.ts
@@ -11,13 +11,26 @@ export class NgxAutocomPlaceDirective {
   @Input() options: google.maps.places.AutocompleteOptions;
 
   constructor(public el: ElementRef) {
+    this.process();
+  }
+
+  async process() {
     const options = this.options
       ? this.options
       : {
           types: ["geocode"] // 'establishment' / 'address' / 'geocode'
         };
+
+    let inputElement;
+    if(this.el.nativeElement instanceof HTMLInputElement) {
+      inputElement = this.el.nativeElement
+    } else if(this.el.nativeElement.tagName === 'ION-INPUT') {
+      inputElement = await this.el.nativeElement.getInputElement()
+    } else {
+      return;
+    }
     const autocomplete = new google.maps.places.Autocomplete(
-      this.el.nativeElement,
+      inputElement,
       options
     );
     google.maps.event.addListener(autocomplete, "place_changed", () => {
